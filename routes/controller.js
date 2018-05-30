a8f2ed0f1cadb1ac504dca03bc718b532165c007
fengxiaoming <xiaoming.feng@daxuezhang.com>
2018-05-30 13:56:06 +0800

    全部活动筛选， 审核  淑琴

--- routes/controller.js
+++ routes/controller.js
@@ -1113,6 +1113,9 @@ exports.study_abroad_activity = function (req, res, next) {
     data.zuixinhuodong_public= returnData(result.zuixinhuodong_public,'zuixinhuodong_public');
     data.schoolpaiming_public = returnData(result.schoolpaiming_public,'schoolpaiming_public');*/
    /* data.liuxuehuodong_list = returnData(result.liuxuehuodong_list,'liuxuehuodong_list');*/
+    data.country = 0;
+    data.timetype= 0;
+    data.type='所有学历';
     data.tdk = {
       pagekey: 'ACTIVITY', //key
       cityid: area, //cityid
