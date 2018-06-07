54d5f244a31f2a77f40b57d0635469c92b8e653f
duxiaoli <1151036495@qq.com>
2018-06-07 17:19:06 +0800

    修改代码  审核人：田海亮

--- routes/controller.js
+++ routes/controller.js
@@ -2983,7 +2983,7 @@ exports.getCoupons = function (req, res, next) {
 exports.obtain = function (req, res, next){
     log.debug('活动底页');
     var data = [];
-        data.tdk = {
+    data.tdk = {
         title: '最高减免2000元，要留学的你还不知道？'
     };
     res.render('obtain', data);
